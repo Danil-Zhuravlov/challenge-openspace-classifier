f98a1fda40bfa3acbe5967c9b7a1531130dc949d
Danil Zhuravlov <danil.zhuravlov.2004@gmail.com>
2024-02-08 10:04:59 +0100

    added structure of files and structure of code to work with.

---
+++ utils/openspace.py
@@ -0,0 +1,13 @@
+class Openspace:
+    def __init__(self, tables: list, number_of_tables: int):
+        self.tables = tables
+        self.number_of_tables = number_of_tables
+
+    def organize(self, names):
+        pass
+
+    def display(self):
+        pass
+
+    def store(filename):
+        pass
---
+++ utils/table.py
@@ -0,0 +1,24 @@
+class Seat:
+    def __init__(self, free: bool, occupant: str)
+        self.free = free
+        self.occupant = occupant
+
+    def set_occupant(name):
+        pass
+
+    def remove_occupant():
+        pass
+
+class Table:
+    def __init__(self, capacity: int, seats: list)
+        self.capacity = capacity
+        self.seats = seats
+
+    def has_free_spot() -> bool:
+        pass
+
+    def assign_seat(name):
+        pass
+
+    def left_capacity() -> int:
+        pass
